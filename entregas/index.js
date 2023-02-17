92bbc3c0243b79337e458d9ce8365714b1d6cf74
Selena Cuadra <selenac99@hotmail.com>
2023-02-17 12:16:48 -0300

    finalizando segunda entrega coderhouse

--- entregas/index.js
+++ entregas/index.js
@@ -1,47 +1,65 @@
+const animales = [ {
+    id: 1,
+    nombre: "perro", 
+    cantidad: 5,
+    tipo: "callejeros",
+},
+{
+    id: 2,
+    nombre: "gato", 
+    cantidad: 5,
+    tipo: "callejeros",
+},
+{
+    id: 3,
+    nombre: "serpiente", 
+    cantidad: 0,
+    tipo: "exotico",
+},
+{
+    id: 4,
+    nombre: "loro", 
+    cantidad: 5,
+    tipo: "ave",
+},
+{
+    id: 5,
+    nombre: "lagarto", 
+    cantidad: 6,
+    tipo: "exotico",
+}]
 
-let nombre = prompt("Ingrese su nombre:");
-let apellido = prompt("ingrese su apellido: ");
+let nombres_animales = animales.map(animal => {
+    return animal.nombre
+})
+alert("los animales que tenemos con frecuencia para adoptar son: " + " " + nombres_animales);
 
-
-alert("Bienvenido" + " " + nombre + " " + apellido);
-
-
-
-let dia = prompt("elegi el día que queres saber tus horarios: ");
-
-
-if (dia) {
-    switch (dia)
-    {
-    case "Lunes":
-        alert("Hoy trabajas de 9.00am hasta 17.00pm presencial");
-        break;
-        case "Martes":
-        alert("Hoy trabajas de 9.00am hasta 17.00pm presencial");
-        break;
-        case "Miercoles":
-        alert("Hoy trabajas de 9.00am hasta 17.00pm virtual");
-        break;
-        case "Jueves":
-        alert("Hoy trabajas de 9.00am hasta 15.00pm virtual");
-        break;
-        case "Viernes":
-        alert("Hoy trabajas de 9.00am hasta 16.00pm presencial y a las 18.00pm tenes turno en el doctor");
-        break;
-        case "Sabado":
-        alert("Hoy no trabajas, tenes día libre");
-        break;
-        case "Domingo":
-        alert("Hoy almorzas en lo de tus abuelos");
-        break;
-        default:
-            alert("error")
-            break;
-    
-         } }
+let mascota = prompt("ingrese la mascota que busca: ");
 
 
-         document.write("Agenda de " +  " " + nombre + " " + apellido);
-         document.write('<br>');
-         document.write("El día que elegiste fue " + " " + dia);
+ 
+switch (mascota) {
+  case "lagarto":
+    document.write("Tenemos disponibilidad");
+    break;
+  case "perro":
+    document.write("Tenemos disponibilidad");
+    break;
+  case "gato":
+    document.write("Tenemos disponibilidad");
+    break;
+  case "serpiente":
+    document.write("No tenemos disponibilidad");
+    break;
+  case "loro":
+    document.write("Tenemos disponibilidad");
+    break;
+  default:
+    document.write("No tengo la mascota que busca");
+    break;
+}
+
+
+
+
          
