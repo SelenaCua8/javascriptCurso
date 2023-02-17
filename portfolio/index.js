e601d54c563ff74b86a7224ede9c788de203912f
Selena Cuadra <selenac99@hotmail.com>
2023-02-17 16:15:43 -0300

    termine el portfolio el nav, el sobre mi, el descargar cv, habilidades, el footer y el contactame. Solo queda tecnologias

--- portfolio/index.js
+++ portfolio/index.js
@@ -4,3 +4,25 @@ const navLink = document.querySelector('.nav__link');
 hamburger.addEventListener('click', () => {
   navLink.classList.toggle('hide');
 });
+
+
+//FORM
+window.addEventListener("load",function(){
+	document.formulario.addEventListener("submit",validarFormulario);
+	window.formulario.nombre.addEventListener("input",function(){
+		 document.getElementById("errorNombre").innerHTML= "";
+	})
+
+});		
+
+function validarFormulario(e){
+	e = e || window.event;
+	if(window.formulario.nombre.value==""){
+		e.preventDefault();
+		document.getElementById("errorNombre").innerHTML= "Debes rellenar el campo";
+		document.getElementById("errorNombre").setAttribute("style","color:red;");
+	}else{
+		//podríamos escribir si quisiéramos document.formulario.submit() y forzaríamos el envío pero es innecesario porqué se hará igualmente.
+                console.log("el formulario se ha enviado");
+	}
+}
